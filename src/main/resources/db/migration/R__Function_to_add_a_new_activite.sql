7418d41930248027d154ce7d44defaed6227a36d
Louis Bocquillon <louis.bocquillonliger-belair@iut-rodez.fr>
2019-02-20 16:07:08 +0100

    Fix 2.2 function to add new activity OK

--- src/main/resources/db/migration/R__Function_to_add_a_new_activite.sql
+++ src/main/resources/db/migration/R__Function_to_add_a_new_activite.sql
@@ -1,7 +1,8 @@
 CREATE OR REPLACE FUNCTION add_activity_with_title(title varchar(200)) RETURNS bigint AS $$
    
    INSERT INTO activity (id, title)
-   VALUES (nextval('id_generator'), title);
-   SELECT lastval();
+   VALUES (nextval('id_generator'), add_activity_with_title.title)
+   returning id;
+  -- SELECT lastval(); --> mauvaise idée ! ("belle idée, mais pas bonne idée)
    
 $$ LANGUAGE SQL;
