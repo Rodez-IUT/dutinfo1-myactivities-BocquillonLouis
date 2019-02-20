e6348dbb6b12905c3dae0512eee6514fee02cfa8
Louis Bocquillon <louis.bocquillonliger-belair@iut-rodez.fr>
2019-02-20 15:48:04 +0100

    Fix 2.2 function to add new activity OK

--- src/main/resources/db/migration/R__Function_to_delete_old_activities.sql
+++ src/main/resources/db/migration/R__Function_to_delete_old_activities.sql
@@ -1,8 +1,7 @@
-CREATE OR REPLACE FUNCTION delete_activities_older_than(old_date date) RETURNS int AS $$
+CREATE OR REPLACE FUNCTION delete_activities_older_than(old_date date) RETURNS void AS $$
    
    DELETE
    FROM activity
    WHERE modification_date < old_date;
-   SELECT 1;
    
 $$ LANGUAGE SQL;
