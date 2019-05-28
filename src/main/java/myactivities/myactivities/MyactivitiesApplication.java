5235a00139ea8ff1e18fba95f9863b07d4743423
Louis Bocquillon <louis.bocquillonliger-belair@iut-rodez.fr>
2019-05-28 09:42:46 +0200

    GIT Fix 8.1.2 Project compiles.

--- src/main/java/myactivities/myactivities/MyactivitiesApplication.java
+++ src/main/java/myactivities/myactivities/MyactivitiesApplication.java
@@ -3,9 +3,18 @@ package myactivities.myactivities;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * TODO commenter les responsabilités de cette classe
+ * @author louis.bocquillonlige
+ *
+ */
 @SpringBootApplication
 public class MyactivitiesApplication {
 
+    /**
+     * TODO commenter le rôle de cette méthode
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(MyactivitiesApplication.class, args);
     }
